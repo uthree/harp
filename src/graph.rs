c3f85371cca3f399bdf23b655e9f7b7f79b6d0d7
uthree <uthree271828@gmail.com>
2025-06-29 22:18:13 +0900

    tmp

--- src/graph.rs
+++ src/graph.rs
@@ -1,49 +1,13 @@
 use std::cell::RefCell;
 use std::sync::Arc;
 
-#[derive(Debug, Clone)]
-pub enum Operator {
-    // Input node
-    Input,
-
-    // unary
-    Recip,
-    Sin,
-    Log2,
-    Exp2,
-    Sqrt,
-
-    // binary
-    Add,
-    Mul,
-    Mod,
-    LessThan,
-
-    // dimensional
-    Sum(usize),
-    CumSum(usize),
-    Prod(usize),
-    CumProd(usize),
-    Max(usize),
-    ArgMax(usize),
-
-    // shape control
-    Contiguous,
-}
-
-#[derive(Debug, Clone)]
-pub struct Node {
-    operator: Operator,
-    inputs: Vec<Arc<RefCell<Self>>>,
-    parent: Arc<RefCell<Graph>>,
-}
-
-#[derive(Debug, Clone)]
-pub struct Graph {}
+struct Graph {}
 
 impl Graph {
-    pub fn new() -> Arc<RefCell<Graph>> {
-        let graph = Graph {};
-        Arc::new(RefCell::new(graph))
+    // initialize graph
+    fn new() -> Arc<RefCell<Graph>> {
+        let ctx = Graph {};
+        let ptr = Arc::new(RefCell::new(ctx));
+        ptr
     }
 }
---
+++ src/node.rs
@@ -0,0 +1,4 @@
+use std::cell::RefCell;
+use std::sync::Arc;
+
+struct Node {}
