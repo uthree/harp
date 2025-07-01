f8fb3d95598cf348635824e89e20d8222ac477e6
uthree <uthree271828@gmail.com>
2025-07-02 01:28:32 +0900

    add symbolic

--- src/shape/symbolic.rs
+++ src/shape/symbolic.rs
@@ -11,4 +11,215 @@ pub enum Expr {
     Mul(Box<Self>, Box<Self>),
     Div(Box<Self>, Box<Self>),
     Rem(Box<Self>, Box<Self>),
+    Neg(Box<Self>),
+}
+
+impl Expr {
+    pub fn simplify(self) -> Self {
+        match self {
+            Expr::Int(_) | Expr::Var(_) => self,
+            Expr::Add(a, b) => {
+                let a = a.simplify();
+                let b = b.simplify();
+                match (a, b) {
+                    (Expr::Int(a), Expr::Int(b)) => Expr::Int(a + b),
+                    (expr, Expr::Int(0)) | (Expr::Int(0), expr) => expr,
+                    (a, b) => Expr::Add(Box::new(a), Box::new(b)),
+                }
+            }
+            Expr::Sub(a, b) => {
+                let a = a.simplify();
+                let b = b.simplify();
+                match (a, b) {
+                    (Expr::Int(a), Expr::Int(b)) => Expr::Int(a - b),
+                    (expr, Expr::Int(0)) => expr,
+                    (a, b) if a == b => Expr::Int(0),
+                    (a, b) => Expr::Sub(Box::new(a), Box::new(b)),
+                }
+            }
+            Expr::Mul(a, b) => {
+                let a = a.simplify();
+                let b = b.simplify();
+                match (a, b) {
+                    (Expr::Int(a), Expr::Int(b)) => Expr::Int(a * b),
+                    (_, Expr::Int(0)) | (Expr::Int(0), _) => Expr::Int(0),
+                    (expr, Expr::Int(1)) | (Expr::Int(1), expr) => expr,
+                    (a, b) => Expr::Mul(Box::new(a), Box::new(b)),
+                }
+            }
+            Expr::Div(a, b) => {
+                let a = a.simplify();
+                let b = b.simplify();
+                match (a, b) {
+                    (Expr::Int(a), Expr::Int(b)) if b != 0 => Expr::Int(a / b),
+                    (Expr::Int(0), _) => Expr::Int(0),
+                    (expr, Expr::Int(1)) => expr,
+                    (a, b) if a == b => Expr::Int(1),
+                    (a, b) => Expr::Div(Box::new(a), Box::new(b)),
+                }
+            }
+            Expr::Rem(a, b) => {
+                let a = a.simplify();
+                let b = b.simplify();
+                match (a, b) {
+                    (Expr::Int(a), Expr::Int(b)) if b != 0 => Expr::Int(a % b),
+                    (Expr::Int(0), _) => Expr::Int(0),
+                    (_, Expr::Int(1)) => Expr::Int(0),
+                    (a, b) => Expr::Rem(Box::new(a), Box::new(b)),
+                }
+            }
+            Expr::Neg(expr) => match expr.simplify() {
+                Expr::Int(val) => Expr::Int(-val),
+                Expr::Neg(inner_expr) => *inner_expr,
+                e => Expr::Neg(Box::new(e)),
+            },
+        }
+    }
+}
+
+impl Add for Expr {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self::Add(Box::new(self), Box::new(rhs))
+    }
+}
+
+impl Sub for Expr {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self::Sub(Box::new(self), Box::new(rhs))
+    }
+}
+
+impl Mul for Expr {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        Self::Mul(Box::new(self), Box::new(rhs))
+    }
+}
+
+impl Div for Expr {
+    type Output = Self;
+
+    fn div(self, rhs: Self) -> Self::Output {
+        Self::Div(Box::new(self), Box::new(rhs))
+    }
+}
+
+impl Rem for Expr {
+    type Output = Self;
+
+    fn rem(self, rhs: Self) -> Self::Output {
+        Self::Rem(Box::new(self), Box::new(rhs))
+    }
+}
+
+impl Neg for Expr {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        Self::Neg(Box::new(self))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add_simplify() {
+        let expr = Expr::Int(1) + Expr::Int(2);
+        assert_eq!(expr.simplify(), Expr::Int(3));
+
+        let expr = Expr::Var("x".to_string()) + Expr::Int(0);
+        assert_eq!(expr.simplify(), Expr::Var("x".to_string()));
+
+        let expr = Expr::Int(0) + Expr::Var("x".to_string());
+        assert_eq!(expr.simplify(), Expr::Var("x".to_string()));
+    }
+
+    #[test]
+    fn test_sub_simplify() {
+        let expr = Expr::Int(3) - Expr::Int(2);
+        assert_eq!(expr.simplify(), Expr::Int(1));
+
+        let expr = Expr::Var("x".to_string()) - Expr::Int(0);
+        assert_eq!(expr.simplify(), Expr::Var("x".to_string()));
+
+        let expr = Expr::Var("x".to_string()) - Expr::Var("x".to_string());
+        assert_eq!(expr.simplify(), Expr::Int(0));
+    }
+
+    #[test]
+    fn test_mul_simplify() {
+        let expr = Expr::Int(2) * Expr::Int(3);
+        assert_eq!(expr.simplify(), Expr::Int(6));
+
+        let expr = Expr::Var("x".to_string()) * Expr::Int(1);
+        assert_eq!(expr.simplify(), Expr::Var("x".to_string()));
+
+        let expr = Expr::Int(1) * Expr::Var("x".to_string());
+        assert_eq!(expr.simplify(), Expr::Var("x".to_string()));
+
+        let expr = Expr::Var("x".to_string()) * Expr::Int(0);
+        assert_eq!(expr.simplify(), Expr::Int(0));
+
+        let expr = Expr::Int(0) * Expr::Var("x".to_string());
+        assert_eq!(expr.simplify(), Expr::Int(0));
+    }
+
+    #[test]
+    fn test_div_simplify() {
+        let expr = Expr::Int(6) / Expr::Int(3);
+        assert_eq!(expr.simplify(), Expr::Int(2));
+
+        let expr = Expr::Var("x".to_string()) / Expr::Int(1);
+        assert_eq!(expr.simplify(), Expr::Var("x".to_string()));
+
+        let expr = Expr::Int(0) / Expr::Var("x".to_string());
+        assert_eq!(expr.simplify(), Expr::Int(0));
+
+        let expr = Expr::Var("x".to_string()) / Expr::Var("x".to_string());
+        assert_eq!(expr.simplify(), Expr::Int(1));
+    }
+
+    #[test]
+    fn test_rem_simplify() {
+        let expr = Expr::Int(7) % Expr::Int(3);
+        assert_eq!(expr.simplify(), Expr::Int(1));
+
+        let expr = Expr::Var("x".to_string()) % Expr::Int(1);
+        assert_eq!(expr.simplify(), Expr::Int(0));
+
+        let expr = Expr::Int(0) % Expr::Var("x".to_string());
+        assert_eq!(expr.simplify(), Expr::Int(0));
+    }
+
+    #[test]
+    fn test_neg_simplify() {
+        let expr = -Expr::Int(5);
+        assert_eq!(expr.simplify(), Expr::Int(-5));
+
+        let expr = -(-Expr::Var("x".to_string()));
+        assert_eq!(expr.simplify(), Expr::Var("x".to_string()));
+    }
+
+    #[test]
+    fn test_complex_simplify() {
+        let expr = (Expr::Var("x".to_string()) + Expr::Int(0)) * Expr::Int(1);
+        assert_eq!(expr.simplify(), Expr::Var("x".to_string()));
+
+        let expr = (Expr::Var("y".to_string()) - Expr::Var("y".to_string())) + Expr::Int(5);
+        assert_eq!(expr.simplify(), Expr::Int(5));
+
+        let expr = (Expr::Int(10) * Expr::Var("z".to_string())) - (Expr::Var("z".to_string()) * Expr::Int(9));
+        let expected = Expr::Sub(
+            Box::new(Expr::Mul(Box::new(Expr::Int(10)), Box::new(Expr::Var("z".to_string())))),
+            Box::new(Expr::Mul(Box::new(Expr::Var("z".to_string())), Box::new(Expr::Int(9))))
+        );
+        assert_eq!(expr.simplify(), expected.simplify());
+    }
 }
