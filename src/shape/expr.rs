d5cf1d29693f0f34c18df613807d45ae66960fac
uthree <uthree271828@gmail.com>
2025-07-01 17:52:15 +0900

    tmp

--- src/shape/expr.rs
+++ src/shape/expr.rs
@@ -1,8 +1,16 @@
 use std::fmt::Debug;
 
 #[derive(Clone, Debug)]
-pub enum Expr {
-    // literal
+pub struct Expr(Vec<Term>);
+
+#[derive(Clone, Debug)]
+pub struct Term {
+    prod: Vec<Atom>,
+    div: Vec<Atom>,
+}
+
+#[derive(Clone, Debug)]
+pub enum Atom {
     Var(String),
-    Const(i32),
+    Const(isize),
 }
