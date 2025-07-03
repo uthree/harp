57b70397aa38dd5490fa269afb453e00bd5f1fed
uthree <uthree271828@gmail.com>
2025-07-04 04:22:15 +0900

    fix

--- examples/tmp/src/main.rs
+++ examples/tmp/src/main.rs
@@ -4,6 +4,6 @@ fn main() {
     let mut graph = Graph::new();
     let a = graph.input(s![1], DataType::Float32);
     let b = graph.input(s![1], DataType::Float32);
-    let c = a + b;
+    let c = a.clone() + b.clone() + a.clone();
     println!("{:?}", c)
 }
