81a4733c6553b7dfa361f8e4a7c29cbaf62f54bb
uthree <uthree271828@gmail.com>
2025-07-04 20:37:27 +0900

    add op enum

--- src/operator.rs
+++ src/operator.rs
@@ -2,8 +2,24 @@ use std::fmt::Debug;
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
 pub enum Operator {
+    // Input node
     Input,
+
+    // Unary Operators
+    Recip,
+    Sin,
+    Exp2,
+    Log2,
+    Sqrt,
+
+    // Binary Operators
     Add,
     Mul,
-    Recip,
+    Rem,
+    LessThan,
+
+    // Dimensional Operators
+    SumReduce(usize),
+    MaxReduce(usize),
+    Contiguous,
 }
