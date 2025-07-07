fde399738bc6d091a9c3dc0e9df657211fc08cfb
uthree <uthree271828@gmail.com>
2025-07-07 09:35:00 +0900

    tmp

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
+pub mod graph;
 pub mod macros;
+pub mod operator;
 pub mod shape;
+pub mod tensor;
 
 pub mod prelude {
+    pub use crate::graph::Graph;
+    pub use crate::operator::Operator;
     pub use crate::shape::tracker::ShapeTracker;
 }
