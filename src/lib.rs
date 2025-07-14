f7bb83411eb99b9e10a72c54d654925085b66b05
uthree <uthree271828@gmail.com>
2025-07-14 18:14:04 +0900

    add dtype

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,2 @@
+pub mod dtype;
 pub mod uop;
--- src/uop.rs
+++ src/uop.rs
@@ -1,14 +1,26 @@
+use crate::dtype::DType;
 use std::rc::Rc;
 
 #[derive(Clone, Debug, PartialEq)]
 enum Ops {
     Const(ConstantNumber),
+
+    // mathematical operators
     Add(UOp, UOp),
     Mul(UOp, UOp),
+    Rem(UOp, UOp),
+    Recip(UOp),
+    Sqrt(UOp),
+    Sin(UOp),
+    Log2(UOp),
+    Exp2(UOp),
 }
 
 #[derive(Clone, Debug, PartialEq)]
-pub struct UOp(Rc<Ops>);
+pub struct UOp {
+    rc: Rc<Ops>,
+    dtype: DType,
+}
 
 #[derive(Clone, Debug, PartialEq)]
 enum ConstantNumber {
@@ -20,8 +32,6 @@ enum ConstantNumber {
     I16(i16),
     I32(i32),
     I64(i64),
-    Usize(usize),
-    Isize(isize),
     F32(f32),
     F64(f64),
 }
@@ -30,7 +40,10 @@ macro_rules! impl_from_for_uop {
     ($from_type:ty, $variant:ident) => {
         impl From<$from_type> for UOp {
             fn from(value: $from_type) -> Self {
-                UOp(Rc::new(Ops::Const(ConstantNumber::$variant(value))))
+                UOp {
+                    rc: Rc::new(Ops::Const(ConstantNumber::$variant(value))),
+                    dtype: DType::$variant,
+                }
             }
         }
     };
@@ -44,54 +57,46 @@ impl_from_for_uop!(i8, I8);
 impl_from_for_uop!(i16, I16);
 impl_from_for_uop!(i32, I32);
 impl_from_for_uop!(i64, I64);
-impl_from_for_uop!(usize, Usize);
-impl_from_for_uop!(isize, Isize);
 impl_from_for_uop!(f32, F32);
 impl_from_for_uop!(f64, F64);
 
-impl std::ops::Add for UOp {
+// operator implementations
+
+impl<T> std::ops::Add<T> for UOp
+where
+    T: Into<UOp>,
+{
     type Output = UOp;
-    fn add(self, rhs: Self) -> Self::Output {
-        UOp(Rc::new(Ops::Add(self, rhs)))
+    fn add(self, rhs: T) -> Self::Output {
+        UOp {
+            rc: Rc::new(Ops::Add(self.clone(), rhs.into())),
+            dtype: self.dtype,
+        }
     }
 }
 
-impl std::ops::Mul for UOp {
+impl<T> std::ops::Mul<T> for UOp
+where
+    T: Into<UOp>,
+{
     type Output = UOp;
-    fn mul(self, rhs: Self) -> Self::Output {
-        UOp(Rc::new(Ops::Mul(self, rhs)))
+    fn mul(self, rhs: T) -> Self::Output {
+        UOp {
+            rc: Rc::new(Ops::Mul(self.clone(), rhs.into())),
+            dtype: self.dtype,
+        }
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    macro_rules! test_from_for_uop {
-        ($test_name:ident, $from_type:ty, $variant:ident, $value:expr) => {
-            #[test]
-            fn $test_name() {
-                let uop: UOp = ($value as $from_type).into();
-                assert_eq!(
-                    uop,
-                    UOp(Rc::new(Ops::Const(ConstantNumber::$variant(
-                        $value as $from_type
-                    ))))
-                );
-            }
-        };
+impl<T> std::ops::Rem<T> for UOp
+where
+    T: Into<UOp>,
+{
+    type Output = UOp;
+    fn rem(self, rhs: T) -> Self::Output {
+        UOp {
+            rc: Rc::new(Ops::Rem(self.clone(), rhs.into())),
+            dtype: self.dtype,
+        }
     }
-
-    test_from_for_uop!(test_from_u8, u8, U8, 8);
-    test_from_for_uop!(test_from_u16, u16, U16, 16);
-    test_from_for_uop!(test_from_u32, u32, U32, 32);
-    test_from_for_uop!(test_from_u64, u64, U64, 64);
-    test_from_for_uop!(test_from_i8, i8, I8, -8);
-    test_from_for_uop!(test_from_i16, i16, I16, -16);
-    test_from_for_uop!(test_from_i32, i32, I32, -32);
-    test_from_for_uop!(test_from_i64, i64, I64, -64);
-    test_from_for_uop!(test_from_usize, usize, Usize, 128);
-    test_from_for_uop!(test_from_isize, isize, Isize, -128);
-    test_from_for_uop!(test_from_f32, f32, F32, 3.2);
-    test_from_for_uop!(test_from_f64, f64, F64, 6.4);
 }
