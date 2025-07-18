d8008e66141a6546daffd2021a3165aee4f14a57
uthree <uthree271828@gmail.com>
2025-07-18 17:05:08 +0900

    Revert "add backend, kernel, etc..."
    This reverts commit 7472a225b1eb9f83a45c61f8948669e79488bb76.

--- src/kernel.rs
+++
@@ -1,5 +0,0 @@
-use crate::variable::Variable;
-
-pub trait Kernel {
-    fn exec(&self, args: &[&dyn Variable]);
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,5 @@
-pub mod backend;
 pub mod dtype;
-pub mod kernel;
 pub mod pattern;
 pub mod shapetracker;
 pub mod tensor;
 pub mod uop;
-pub mod variable;
