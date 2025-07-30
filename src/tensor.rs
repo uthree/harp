49ceb83889240fb42a14b832b12c2223317088b1
uthree <uthree271828@gmail.com>
2025-07-31 06:51:51 +0900

    add tensorop

--- src/tensor.rs
+++ src/tensor.rs
@@ -1,7 +1,11 @@
+use crate::uop::{Const, DType, Op, UOp};
 use std::rc::Rc;
 
 #[derive(Debug, Clone, PartialEq)]
-pub struct TensorData {}
+pub struct TensorData {
+    op: TensorOp,
+    dtype: DType,
+}
 
 #[derive(Debug, Clone)]
 pub struct Tensor {
@@ -12,4 +16,12 @@ impl PartialEq for Tensor {
     fn eq(&self, other: &Self) -> bool {
         Rc::ptr_eq(&self.data, &other.data)
     }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum TensorOp {
+    Elementwise(Op),
+    Reduce(Op, usize),
+    Cumulative(Op, usize),
+    Contiguous,
 }
