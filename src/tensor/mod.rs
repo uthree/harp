850055325f969dd5bc55f478800a87ed93fd2402
uthree <uthree271828@gmail.com>
2025-08-09 13:03:10 +0900

    tmp

--- src/tensor/mod.rs
+++ src/tensor/mod.rs
@@ -1,1 +1,11 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
 use crate::backend::Buffer;
+
+pub struct TensorData {
+    buffer: Option<Box<dyn Buffer>>,
+    requires_grad: bool,
+}
+
+pub struct Tensor(Rc<RefCell<TensorData>>);
