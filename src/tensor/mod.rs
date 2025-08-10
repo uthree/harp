6362b26f1ed793cbc0b8e635bb1e1447a08b75d1
uthree <uthree271828@gmail.com>
2025-08-10 14:11:17 +0900

    Tensor

--- src/tensor/mod.rs
+++ src/tensor/mod.rs
@@ -1,1 +1,15 @@
+use std::cell::RefCell;
+use std::rc::Rc;
 
+use crate::cbuffer::CBuffer;
+
+pub enum TensorBuffer {
+    C(CBuffer),
+}
+
+pub struct TensorData {
+    grad: Option<Tensor>,
+    requires_grad: bool,
+}
+
+pub struct Tensor(Rc<RefCell<TensorData>>);
