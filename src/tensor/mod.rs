25f18e392ba0b1e01c937bcd014380b955759ae4
uthree <uthree271828@gmail.com>
2025-08-10 17:11:51 +0900

    TensorAPIの機能を一部実装

--- src/tensor/mod.rs
+++ src/tensor/mod.rs
@@ -1,24 +1,60 @@
-use std::cell::RefCell;
-
 use crate::backend::Backend;
 use crate::backend::c::CBackend;
 use crate::cbuffer::CBuffer;
+use std::cell::RefCell;
+use std::rc::Rc;
 
 thread_local! {
-    pub static C_BACKEND: RefCell<CBackend> = RefCell::new(CBackend::new());
+    // Store an Rc in the thread local, so we can share it.
+    static C_BACKEND: Rc<RefCell<CBackend>> = Rc::new(RefCell::new(CBackend::new()));
 }
 
 pub enum TensorBuffer {
     C(CBuffer),
 }
-
 pub enum TensorBackend {
-    C(CBackend),
+    C(Rc<RefCell<CBackend>>),
+}
+
+pub enum TensorOp {
+    Rand(Shape),
+    Add(Tensor, Tensor),
 }
 
 pub struct TensorData {
+    op: TensorOp,
     grad: Option<Tensor>,
     requires_grad: bool,
+    backend: TensorBackend,
 }
 
-pub struct Tensor(std::rc::Rc<RefCell<TensorData>>);
+pub type Shape = Vec<usize>;
+
+pub struct Tensor(Rc<RefCell<TensorData>>);
+
+impl Tensor {
+    pub fn rand(shape: Shape) -> Self {
+        TensorData {
+            op: TensorOp::Rand(shape),
+            grad: None,
+            requires_grad: false,
+            backend: backend("c"),
+        }
+        .into()
+    }
+}
+
+impl From<TensorData> for Tensor {
+    fn from(value: TensorData) -> Self {
+        Tensor(Rc::new(RefCell::new(value)))
+    }
+}
+
+// By using C_BACKEND.with, we get a reference to the thread-local value
+// and return a cloned Rc.
+pub fn backend(name: &str) -> TensorBackend {
+    match name {
+        "c" => C_BACKEND.with(|backend| TensorBackend::C(backend.clone())),
+        _ => panic!("Unsupported backend: {}", name),
+    }
+}
