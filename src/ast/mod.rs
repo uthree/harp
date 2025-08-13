e720f72a2d94bc8a88ba0215745084f97f7687b9
uthree <uthree271828@gmail.com>
2025-08-14 08:45:29 +0900

    ast ops

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -1,3 +1,6 @@
+use std::env::Args;
+use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Rem, RemAssign, Sub, SubAssign};
+
 #[derive(Debug, Clone, PartialEq)]
 pub enum DType {
     F32,   // float
@@ -17,6 +20,8 @@ pub enum Const {
 pub enum AstOp {
     Const(Const),
     Add,
+    Mul,
+    Max,
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -25,3 +30,38 @@ pub struct AstNode {
     args: Vec<AstNode>,
     dtype: DType,
 }
+
+impl AstNode {
+    fn _new(op: AstOp, args: Vec<AstNode>, dtype: DType) -> Self {
+        Self { op, args, dtype }
+    }
+}
+
+macro_rules! impl_from_num_for_astnode {
+    ($(($t:ty, $v: ident)),*) => {
+        $(
+            impl From<$t> for AstNode {
+                fn from(n: $t) -> Self {
+                    AstNode::_new(AstOp::Const(Const::$v(n)), vec![], DType::$v)
+                }
+            }
+        )*
+    };
+}
+
+impl_from_num_for_astnode!((usize, Usize));
+
+macro_rules! impl_astnode_binary_op {
+    ($trait:ident, $fname:ident, $variant:ident) => {
+        impl<T: Into<AstNode>> $trait<T> for AstNode {
+            type Output = AstNode;
+            fn $fname(self, rhs: T) -> Self::Output {
+                let dtype = (&self.dtype).clone();
+                AstNode::_new(AstOp::$variant, vec![self, rhs.into()], dtype)
+            }
+        }
+    };
+}
+
+impl_astnode_binary_op!(Add, add, Add);
+impl_astnode_binary_op!(Mul, mul, Mul);
