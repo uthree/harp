b4c472096c73c1e046356492d2bd9c01268eb16a
uthree <uthree271828@gmail.com>
2025-08-14 13:13:55 +0900

    astパターンのマクロを実装

--- src/ast/pattern.rs
+++ src/ast/pattern.rs
@@ -5,6 +5,7 @@ use crate::ast::AstNode;
 pub struct RewriteRule {
     pattern: AstNode,
     rewriter: Box<dyn Fn(&[AstNode]) -> AstNode>,
+    /// A closure that takes captured nodes and returns true if the rewrite should be applied.
     condition: Box<dyn Fn(&[AstNode]) -> bool>,
 }
 
@@ -22,7 +23,46 @@ impl RewriteRule {
     }
 }
 
+/// A macro to create a `RewriteRule`.
+///
+/// # Example
+///
+/// The following example is ignored because `astpat!` is not exported.
+/// ```ignore
+/// use crate::ast::AstNode;
+/// use crate::ast::pattern::RewriteRule;
+///
+/// // without condition
+/// let rule = astpat!(|a| a + 1 => a);
+///
+/// // with condition
+/// let rule = astpat!(|a, b| a + b, if a == b => b + a);
+/// ```
 macro_rules! astpat {
+    (| $($capture: pat_param),* | $pattern: expr, if $condition: expr => $rewriter: expr) => {
+        {
+            let mut counter = 0..;
+            $(
+                let $capture = AstNode::capture(counter.next().unwrap());
+            )*
+            let pattern = $pattern;
+            let rewriter = |captured_nodes: &[AstNode]| {
+                let mut counter = 0..;
+                $(
+                    let $capture = captured_nodes[counter.next().unwrap()].clone();
+                )*
+                $rewriter
+            };
+            let condition = |captured_nodes: &[AstNode]| {
+                let mut counter = 0..;
+                $(
+                    let $capture = captured_nodes[counter.next().unwrap()].clone();
+                )*
+                $condition
+            };
+            RewriteRule::new(pattern, rewriter, condition)
+        }
+    };
     (| $($capture: pat_param),* | $pattern: expr => $rewriter: expr ) => {
         {
             let mut counter = 0..;
@@ -45,9 +85,4 @@ macro_rules! astpat {
 pub struct AstRewriter {
     name: String,
     rules: Vec<Rc<RewriteRule>>,
-}
-
-#[test]
-fn test_astpat_macro() {
-    let pat = astpat!(|a| a => a);
 }
