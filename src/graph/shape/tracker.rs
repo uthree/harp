0eb17a6340b63ffb320e86049f340d9db67d7cd3
uthree <uthree271828@gmail.com>
2025-08-15 18:24:16 +0900

    基本的なView操作を実装

--- src/graph/shape/tracker.rs
+++ src/graph/shape/tracker.rs
@@ -1,4 +1,5 @@
 use crate::graph::shape::expr::Expr;
+use std::collections::HashSet;
 
 // ShapeTrackerの責務は、テンソルの各次元の添字からメモリオフセットへの変換を数式(ExprやAstNode)として表現することです。
 // 簡略化のためASTではなくshape::Exprで計算されますが、ExprはASTに簡単に変換することができるので実用上の問題は特にありません。
@@ -11,18 +12,93 @@ pub enum View {
         strides: Vec<Expr>, // 各次元の添え字の係数
         offset: Expr,       // オフセット
     },
+    // 非線形な場合はとりあえず保留する。
 }
 
 impl View {
-    fn new_continuous(shape: Vec<Expr>) -> Self {
+    pub fn new_continuous(shape: Vec<Expr>) -> Self {
         let mut strides = vec![Expr::from(1); shape.len()];
         for i in (0..shape.len() - 1).rev() {
-            strides[i] = strides[i + 1].clone() * shape[i + 1].clone();
+            strides[i] = (strides[i + 1].clone() * shape[i + 1].clone()).simplify();
         }
         View::Linear {
             shape,
             strides,
             offset: Expr::from(0),
+        }
+    }
+
+    pub fn ndim(&self) -> usize {
+        match self {
+            View::Linear { shape, .. } => shape.len(),
+        }
+    }
+
+    pub fn permute(self, axes: Vec<usize>) -> Self {
+        assert!(self.ndim() == axes.len());
+        let axes_set: HashSet<_> = axes.iter().collect();
+        assert!(axes_set.len() == axes.len(), "duplicate axis in permute");
+        match self {
+            View::Linear {
+                shape,
+                strides,
+                offset,
+            } => {
+                let mut new_shape = vec![];
+                let mut new_strides = vec![];
+                for axis in axes.iter() {
+                    new_shape.push(shape[*axis].clone().simplify());
+                    new_strides.push(strides[*axis].clone().simplify());
+                }
+                View::Linear {
+                    shape: new_shape,
+                    strides: new_strides,
+                    offset,
+                }
+            }
+        }
+    }
+
+    pub fn unsqueeze(self, axis: usize) -> Self {
+        assert!(axis <= self.ndim());
+        match self {
+            View::Linear {
+                shape,
+                strides,
+                offset,
+            } => {
+                let mut shape = shape.clone();
+                let mut strides = strides.clone();
+                shape.insert(axis, 1.into());
+                strides.insert(axis, 0.into());
+                View::Linear {
+                    shape,
+                    strides,
+                    offset,
+                }
+            }
+        }
+    }
+
+    pub fn squeeze(self, axis: usize) -> Self {
+        assert!(axis < self.ndim());
+        match self {
+            View::Linear {
+                shape,
+                strides,
+                offset,
+            } => {
+                let mut shape = shape.clone();
+                let mut strides = strides.clone();
+                assert_eq!(shape[axis], 1.into(), "can only squeeze an axis of size 1");
+                shape.remove(axis);
+                strides.remove(axis);
+                View::Linear {
+                    shape: shape,
+                    strides: strides,
+                    offset: offset,
+                }
+            }
         }
     }
 }
