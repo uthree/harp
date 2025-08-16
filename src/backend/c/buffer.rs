de91a6e818e90438c0f282842a0a98aa367ae774
uthree <uthree271828@gmail.com>
2025-08-16 18:31:40 +0900

    tmp

--- src/backend/c/buffer.rs
+++ src/backend/c/buffer.rs
@@ -2,4 +2,11 @@ use crate::ast::DType;
 use crate::backend::Buffer;
 
 #[derive(Debug)]
-pub struct CBuffer {}
+pub struct CBuffer {
+    /// Raw pointer to the allocated memory.
+    pub ptr: *mut c_void,
+    /// The shape of the buffer.
+    pub shape: Vec<usize>,
+    /// The data type of the elements.
+    pub dtype: DType,
+}
