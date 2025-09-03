dd71b4a38fe8334de55211f9134758d9b8455e61
uthree <uthree271828@gmail.com>
2025-09-03 16:23:45 +0900

    各種GraphOpについての記述を追加

--- src/graph/mod.rs
+++ src/graph/mod.rs
@@ -1,6 +1,6 @@
 use std::rc::Rc;
 
-use crate::ast::{AstNode, DType};
+use crate::ast::{AstNode, ConstLiteral, DType};
 pub mod shape;
 use crate::graph::shape::Expr as ShapeExpr;
 
@@ -26,7 +26,17 @@ pub struct TensorSignature {
     // ちなみにViewに関しては、入出力の時点では常にContiguousであるとする。
 }
 
-pub enum ElementwiseOp {}
+pub enum ElementwiseOp {
+    Add,
+    Mul,
+    Max,
+    Recip,
+    Neg,
+    Sqrt,
+    Log2,
+    Exp2,
+    Sin,
+}
 
 pub enum ReduceOp {
     Add,
@@ -35,13 +45,22 @@ pub enum ReduceOp {
 }
 
 pub enum GraphOp {
-    FusedElementwise(AstNode), // capture(n) が src[n]に対応する
-    FusedReduce(Vec<usize>),   // 1つ以上の軸をReduce
+    Const(ConstLiteral, Vec<ShapeExpr>), // 定数ノード, 任意のshapeの定数
+    Rand(Vec<ShapeExpr>),                // 任意のshapeの一様乱数を生成
+    Arange(usize), // 任意の長さの１次元Tensorを生成、 内容は [0, 1, 2, ..., n],
+    Reshape(Vec<ShapeExpr>),
+    Elementwise(AstNode),         // Capture(n) が src[n]に対応する要素ごとの演算
+    Reduce(ReduceOp, Vec<usize>), // 1つ以上の軸をReduce
+    ElemenWiseReduce(AstNode, ReduceOp, Vec<usize>), //element-wiseな演算をしながらReduceする
+    Cumulative(ReduceOp, usize),  // 累積演算
+    ElementwiseCumulative(AstNode, ReduceOp, usize), // Element-wise演算をしつつ累積する
+    Contiguous,                   // 連続なメモリ配置に並べ直す
 }
 
 pub struct GraphNodeData {
     op: GraphOp,
     src: Vec<GraphNode>,
+    dtype: DType,
 }
 
 pub struct GraphNode(Rc<GraphNodeData>);
