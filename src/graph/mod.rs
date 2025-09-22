2342914d448c8897f6c423ce9c4dec4cef01cf6d
uthree <uthree271828@gmail.com>
2025-09-22 10:37:44 +0900

    アリーナアロケータ形式のグラフを実装開始

--- src/graph/mod.rs
+++ src/graph/mod.rs
@@ -7,9 +7,69 @@ use crate::graph::shape::{view::View, Expr as ShapeExpr};
 #[derive(Debug)]
 pub struct Graph {
     nodes: Vec<GraphNode>,
+    inputs: Vec<NodeId>,
+    outputs: Vec<NodeId>,
 }
 
 impl Graph {
+    pub fn new() -> Self {
+        Self {
+            nodes: Vec::new(),
+            inputs: Vec::new(),
+            outputs: Vec::new(),
+        }
+    }
+
+    pub fn add_node(&mut self, op: GraphOp, inputs: Vec<NodeId>) -> NodeId {
+        let id = NodeId(self.nodes.len());
+        self.nodes.push(GraphNode { op, inputs });
+        id
+    }
+
+    pub fn add_input(&mut self) -> NodeId {
+        let id = self.add_node(GraphOp::Input, vec![]);
+        self.inputs.push(id);
+        id
+    }
+
+    pub fn add_output(&mut self, node_id: NodeId) {
+        self.outputs.push(node_id);
+    }
+
+    pub fn get_node(&self, id: NodeId) -> Option<&GraphNode> {
+        self.nodes.get(id.0)
+    }
+
+    pub fn get_node_mut(&mut self, id: NodeId) -> Option<&mut GraphNode> {
+        self.nodes.get_mut(id.0)
+    }
+
+    pub fn inputs(&self) -> &[NodeId] {
+        &self.inputs
+    }
+
+    pub fn outputs(&self) -> &[NodeId] {
+        &self.outputs
+    }
+
+    pub fn node_inputs(&self, id: NodeId) -> Option<&[NodeId]> {
+        self.get_node(id).map(|node| &node.inputs[..])
+    }
+
+    pub fn add_cast(&mut self, input: NodeId, dtype: DType) -> NodeId {
+        self.add_node(GraphOp::Cast(dtype), vec![input])
+    }
+
+    pub fn add_add(&mut self, lhs: NodeId, rhs: NodeId) -> NodeId {
+        let op = GraphOp::Elementwise(ElementwiseOp::Add(lhs, rhs));
+        self.add_node(op, vec![lhs, rhs])
+    }
+
+    pub fn add_neg(&mut self, input: NodeId) -> NodeId {
+        let op = GraphOp::Elementwise(ElementwiseOp::Neg(input));
+        self.add_node(op, vec![input])
+    }
+
     fn signature(&self) -> GraphSignature {
         todo!()
     }
@@ -18,10 +78,17 @@ impl Graph {
 #[derive(Debug)]
 pub struct GraphNode {
     op: GraphOp,
+    inputs: Vec<NodeId>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct NodeId(usize);
+
+impl NodeId {
+    pub fn index(self) -> usize {
+        self.0
+    }
+}
 
 #[derive(Debug)]
 pub enum GraphOp {
@@ -31,7 +98,10 @@ pub enum GraphOp {
 }
 
 #[derive(Debug)]
-pub enum ElementwiseOp {}
+pub enum ElementwiseOp {
+    Add(NodeId, NodeId),
+    Neg(NodeId),
+}
 
 #[derive(Default, Debug, Clone, PartialEq)]
 pub struct GraphSignature {
@@ -50,4 +120,40 @@ pub struct ShapeVariableSignature {
 pub struct TensorSignature {
     pub dtype: DType,
     pub shape: Vec<ShapeExpr>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_graph_construction() {
+        let mut graph = Graph::new();
+
+        // 入力ノードを2つ作成
+        let input1 = graph.add_input();
+        let input2 = graph.add_input();
+
+        // 加算ノードを作成
+        let add_node = graph.add_add(input1, input2);
+
+        // 否定ノードを作成
+        let neg_node = graph.add_neg(add_node);
+
+        // 出力として登録
+        graph.add_output(neg_node);
+
+        // 構造の確認
+        assert_eq!(graph.inputs().len(), 2);
+        assert_eq!(graph.outputs().len(), 1);
+        assert_eq!(graph.nodes.len(), 4);
+
+        // add_nodeの入力確認
+        let add_inputs = graph.node_inputs(add_node).unwrap();
+        assert_eq!(add_inputs, &[input1, input2]);
+
+        // neg_nodeの入力確認
+        let neg_inputs = graph.node_inputs(neg_node).unwrap();
+        assert_eq!(neg_inputs, &[add_node]);
+    }
 }
