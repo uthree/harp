9aeb6d2d94b3b4596c1272e603580461fe5824fe
uthree <uthree271828@gmail.com>
2025-09-22 10:33:05 +0900

    tmp2

--- src/graph/mod.rs
+++ src/graph/mod.rs
@@ -1,11 +1,13 @@
-use std::rc::Rc;
+use std::rc::{Rc, Weak};
 
 use crate::ast::{AstNode, ConstLiteral, DType};
 pub mod shape;
 use crate::graph::shape::{view::View, Expr as ShapeExpr};
 
 #[derive(Debug)]
-pub struct Graph {}
+pub struct Graph {
+    nodes: Vec<GraphNode>,
+}
 
 impl Graph {
     fn signature(&self) -> GraphSignature {
@@ -13,14 +15,23 @@ impl Graph {
     }
 }
 
-pub struct GraphNodeData {
+#[derive(Debug)]
+pub struct GraphNode {
     op: GraphOp,
 }
-pub struct GraphNode(Rc<GraphNodeData>);
 
+#[derive(Debug)]
+pub struct NodeId(usize);
+
+#[derive(Debug)]
 pub enum GraphOp {
-    Input { dtype: DType, shape: Vec<ShapeExpr> },
+    Input,
+    Elementwise(ElementwiseOp), // apply element-wise operation
+    Cast(DType),                // convert type
 }
+
+#[derive(Debug)]
+pub enum ElementwiseOp {}
 
 #[derive(Default, Debug, Clone, PartialEq)]
 pub struct GraphSignature {
