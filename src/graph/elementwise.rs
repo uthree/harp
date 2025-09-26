e886d2e5ad279f8cb2949d7a542acdb70b73316a
uthree <uthree271828@gmail.com>
2025-09-26 13:07:01 +0900

    単項演算子を追加

--- src/graph/elementwise.rs
+++ src/graph/elementwise.rs
@@ -4,10 +4,16 @@ pub enum ElementwiseOp {
     Add(GraphNode, GraphNode),
     Max(GraphNode, GraphNode),
     Mod(GraphNode, GraphNode),
+    Neg(GraphNode),
+    Recip(GraphNode),
+    Sin(GraphNode),
+    Sqrt(GraphNode),
+    Log2(GraphNode),
+    Exp2(GraphNode),
 }
 
 
-macro_rules! impl_elementwise_op {
+macro_rules! impl_elementwise_binary_op {
     ($trait:ident, $method:ident, $variant:ident) => {
         impl std::ops::$trait for GraphNode {
             type Output = GraphNode;
@@ -29,8 +35,28 @@ macro_rules! impl_elementwise_op {
     };
 }
 
-impl_elementwise_op!(Add, add, Add);
-impl_elementwise_op!(Rem, rem, Mod);
+macro_rules! impl_elementwise_unary_op {
+    ($trait:ident, $method:ident, $variant:ident) => {
+        impl std::ops::$trait for GraphNode {
+            type Output = GraphNode;
+            fn $method(self) -> Self::Output {
+                // 結果のviewはそのまま継承
+                let result_view = self.view.clone();
+
+                // 新しいGraphNodeを作成
+                GraphNode::new(
+                    GraphOp::Elementwise(ElementwiseOp::$variant(self.clone())),
+                    self.dtype.clone(),
+                    result_view,
+                )
+            }
+        }
+    };
+}
+
+impl_elementwise_binary_op!(Add, add, Add);
+impl_elementwise_binary_op!(Rem, rem, Mod);
+impl_elementwise_unary_op!(Neg, neg, Neg);
 
 impl GraphNode {
     pub fn max(self, rhs: Self) -> Self {
@@ -43,6 +69,38 @@ impl GraphNode {
         // 新しいGraphNodeを作成
         GraphNode::new(
             GraphOp::Elementwise(ElementwiseOp::Max(self.clone(), rhs.clone())),
+            self.dtype.clone(),
+            result_view,
+        )
+    }
+
+    pub fn recip(self) -> Self {
+        self.apply_unary_op(ElementwiseOp::Recip)
+    }
+
+    pub fn sin(self) -> Self {
+        self.apply_unary_op(ElementwiseOp::Sin)
+    }
+
+    pub fn sqrt(self) -> Self {
+        self.apply_unary_op(ElementwiseOp::Sqrt)
+    }
+
+    pub fn log2(self) -> Self {
+        self.apply_unary_op(ElementwiseOp::Log2)
+    }
+
+    pub fn exp2(self) -> Self {
+        self.apply_unary_op(ElementwiseOp::Exp2)
+    }
+
+    fn apply_unary_op(self, op_constructor: fn(GraphNode) -> ElementwiseOp) -> Self {
+        // 結果のviewはそのまま継承
+        let result_view = self.view.clone();
+
+        // 新しいGraphNodeを作成
+        GraphNode::new(
+            GraphOp::Elementwise(op_constructor(self.clone())),
             self.dtype.clone(),
             result_view,
         )
