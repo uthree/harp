fc1c9d5f985f8e3f2bba08aff044246d50abaf6b
uthree <uthree271828@gmail.com>
2025-10-16 17:22:15 +0900

    fix

---
+++ src/ast/mod.rs
@@ -0,0 +1,56 @@
+mod helper;
+
+pub use helper::*;
+
+#[derive(Debug, Clone)]
+pub struct AstNode {
+    op: AstOp,
+    dtype: DType,
+}
+
+#[derive(Debug, Clone)]
+pub enum AstOp {
+    Const(ConstValue),
+
+    // arithmetic / math operators
+    Add(Box<AstNode>, Box<AstNode>),
+    Mul(Box<AstNode>, Box<AstNode>),
+    Max(Box<AstNode>, Box<AstNode>),  // take greater one
+    Rem(Box<AstNode>, Box<AstNode>),  // remainder
+    IDiv(Box<AstNode>, Box<AstNode>), // integer division
+    Neg(Box<AstNode>),
+    Recip(Box<AstNode>),
+    Sqrt(Box<AstNode>),
+    Sin(Box<AstNode>),
+    Log2(Box<AstNode>),
+    Exp2(Box<AstNode>),
+    LessThan(Box<AstNode>),
+
+    CallFunction, // TODO: add fields
+
+    // statements (TODO: add fields)
+    Range, // loop
+    Func,  // define the function
+    If,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum DType {
+    Unknown, // unknown type, placeholder
+    None,    // void
+    Isize,   // signed integer
+    Usize,   // unsigned integer
+    F32,     // 32-bit float
+    Bool,    // bool,
+
+    Ptr(Box<DType>),        // pointer of some type
+    Vec(Box<DType>, usize), // fixed-size vector
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum ConstValue {
+    Isize(isize),
+    Usize(usize),
+    F32(f32),
+    Bool(bool),
+}
---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+pub mod ast;
+pub mod graph;
+pub mod opt;
