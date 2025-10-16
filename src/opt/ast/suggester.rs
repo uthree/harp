83fb598563241644f9bd74df3429204cfb4f7f2d
uthree <uthree271828@gmail.com>
2025-10-16 16:46:31 +0900

    reset

--- src/opt/ast/suggester.rs
+++
@@ -1,6 +0,0 @@
-// Re-export suggester, cost estimator, and optimizer implementations from heuristic module
-pub use crate::opt::ast::heuristic::{
-    BeamSearchOptimizer, CommutativeSuggester, CostBasedOptimizer, LoopInterchangeSuggester,
-    LoopTilingSuggester, LoopTransformSuggester, NodeCountCostEstimator, OperationCostEstimator,
-    RuleBasedSuggester,
-};
