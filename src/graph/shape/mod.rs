0a87493f96e0fd1f7f6a0eff42af866373039eab
uthree <uthree271828@gmail.com>
2025-10-22 09:32:23 +0900

    reset

--- src/graph/shape/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod expr;
-pub mod view;
-pub use expr::Expr;
-
-// Re-export the s macro
-pub use crate::s;
--- tests/common/mod.rs
+++
@@ -1,4 +0,0 @@
-pub fn setup() {
-    // Ignore the result because the logger might have been initialized already in another test.
-    let _ = env_logger::try_init();
-}
