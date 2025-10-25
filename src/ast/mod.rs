cd4ac79fe33e0d732560de2f3df175456f41993d
uthree <uthree271828@gmail.com>
2025-10-25 13:41:25 +0900

    tmp

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -1,5 +1,12 @@
-pub mod dtype;
-pub use dtype::DType;
+#[derive(Debug, Clone)]
+pub enum AstNode {
+    // arithmetics
+    Add(Box<AstNode>, Box<AstNode>),
+    Mul(Box<AstNode>, Box<AstNode>),
+    Max(Box<AstNode>, Box<AstNode>),
+    Rem(Box<AstNode>, Box<AstNode>),
+    Ma(Box<AstNode>, Box<AstNode>),
+}
 
 #[derive(Debug, Clone)]
 pub struct AstNode {}
