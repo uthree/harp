0b5eeab51c8ddcbdda01803d60e2cb3a4a1a1ee2
uthree <uthree271828@gmail.com>
2025-10-25 13:41:31 +0900

    fix

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -1,12 +1,25 @@
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 pub enum AstNode {
     // arithmetics
     Add(Box<AstNode>, Box<AstNode>),
     Mul(Box<AstNode>, Box<AstNode>),
     Max(Box<AstNode>, Box<AstNode>),
     Rem(Box<AstNode>, Box<AstNode>),
-    Ma(Box<AstNode>, Box<AstNode>),
+    Idiv(Box<AstNode>, Box<AstNode>),
+    Recip(Box<AstNode>),
+    Sqrt(Box<AstNode>),
+    Log2(Box<AstNode>),
+    Exp2(Box<AstNode>),
+    Sin(Box<AstNode>),
 }
 
 #[derive(Debug, Clone)]
-pub struct AstNode {}
+pub enum DType {
+    Isize,                  // signed integer
+    Usize,                  // unsigned integer (for array indexing)
+    F32,                    // float
+    Ptr(Box<DType>),        // pointer for memory buffer
+    Vec(Box<DType>, usize), // fixed size vector for SIMD
+    Tuple(Vec<DType>),
+    Unknown,
+}
